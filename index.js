1ef998849633fe2b29e7cacfaaa8bf48687d753c
Anurax1321 <jyothiranurag@gmail.com>
2025-03-04 11:57:25 -0600

    title

--- index.js
+++ index.js
@@ -403,7 +403,7 @@ app.get('/dashboard', async (req, res) => {
       </head>
       <body>
         <div class="container-fluid mt-4">
-          <h2 class="dashboard-title">🔹 Log Dashboard</h2>
+          <h2 class="dashboard-title"> Log Dashboard</h2>
           <p class="dashboard-subtitle">
             <strong>File Last Modified:</strong> ${lastModifiedTime} | 
             <strong>Last Updated:</strong> ${new Date().toLocaleString()}

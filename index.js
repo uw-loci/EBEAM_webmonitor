04c5ffdc40ae64f997631f60ac7576db56f1911b
Anurax1321 <jyothiranurag@gmail.com>
2025-04-24 12:09:28 -0500

    accessing within try and catch  block

--- index.js
+++ index.js
@@ -166,11 +166,16 @@ async function extractData() {
   // const temperatureSensor1 = temperatures["1"]; // "18.94"
 
   // You can now use these variables as needed in your front end.
-  console.log('Pressure:', pressure);
-  console.log('Safety Flags:', safetyFlags);
-  console.log('Temperatures:', temperatures['1']);
-  // console.log('Timestamp:', timestamp);
-  // console.log('Temperature from sensor 1:', temperatureSensor1);
+  try {
+    console.log('Pressure:', pressure);
+    console.log('Safety Flags:', safetyFlags);
+    console.log('Temperatures:', temperatures['1']);
+    // console.log('Timestamp:', timestamp);
+    // console.log('Temperature from sensor 1:', temperatureSensor1);
+  } catch (e) {
+    console.log("Error: ", e);
+  }
+
   return data;
 }
 

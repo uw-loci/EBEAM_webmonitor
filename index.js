7ebf71c588bbc9350f3c4f1fe29056fb28ad6077
Pratyush-Shanbhag <pratyushshanbhag10@gmail.com>
2025-05-01 11:28:07 -0500

    Revert "commented out safety flags untill extraction is done"
    This reverts commit ff14ee7b75dbe47bff2152da13ecfcdbada733e7.

--- index.js
+++ index.js
@@ -158,7 +158,7 @@ async function extractData() {
 
     // Accessing each data field:
     const pressure = data.data.pressure; // Access Pressure (e.g., 1200)
-    // const safetyFlags = data.data.safetyFlags[0]; // Access Safety Flags array
+    const safetyFlags = data.data.safetyFlags[0]; // Access Safety Flags array
     const temperatures = data.data.temperatures; // Access Temperatures object
     // const timestamp = response.NEW; // Access the timestamp (or NEW field)
     
@@ -168,7 +168,7 @@ async function extractData() {
 
     // You can now use these variables as needed in your front end.
     console.log('Pressure:', pressure);
-    // console.log('Safety Flags:', safetyFlags);
+    console.log('Safety Flags:', safetyFlags);
     console.log('Temperatures:', temperatures['1']);
     // console.log('Timestamp:', timestamp);
     // console.log('Temperature from sensor 1:', temperatureSensor1);

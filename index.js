0fa3829fdeeddb158e678b5698ca9e95d5f69cfd
Arundhati Singh <arundhatisingh171003@gmail.com>
2025-05-16 13:32:47 -0500

    changed temp to previewContent

--- index.js
+++ index.js
@@ -646,7 +646,7 @@ app.get('/', async (req, res) => {
             max-height: 600px;
             overflow-y: auto;
             font-size: 1.2em;
-            border-radius: 15px;
+            border-radius: 9px;
             margin-top: 2.75em;
             }
 

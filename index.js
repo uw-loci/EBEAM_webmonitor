9428027e697f5a82e731737eae87474daed0852c
Arundhati Singh <arundhatisingh171003@gmail.com>
2025-08-07 13:38:09 -0500

    sic and vtrx must grey out upon ending the experiment

--- index.js
+++ index.js
@@ -1456,12 +1456,12 @@ try {
 
           interlockIds.forEach((id, i) => {
             const elem = document.getElementById(id);
-            if (elem && experimentRunning) elem.style.backgroundColor = data.sicColors[i];
+            if (elem && experimentRunning) elem.style.backgroundColor = experimentRunning ? data.sicColors[i] : 'grey';
           });
 
           vacuumIds.forEach((id, i) => {
             const elem = document.getElementById(id);
-            if (elem && experimentRunning) elem.style.backgroundColor = data.vacuumColors[i];
+            if (elem && experimentRunning) elem.style.backgroundColor = experimentRunning ? data.vacuumColors[i] : 'grey';
           });
 
 
